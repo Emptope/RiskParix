021b375df5ff95c38ab443f4204bc890af659aa1
Tuo Yao <emptope@icloud.com>
2025-04-11 14:24:48 +0800

    fix: delete frontend frame

--- frontend/pages/api/data.js
+++
@@ -1,5 +0,0 @@
-export default async function handler(req, res) {
-  const response = await fetch('http://localhost:8000/api/data'); 
-  const data = await response.json();
-  res.status(200).json(data);
-}
